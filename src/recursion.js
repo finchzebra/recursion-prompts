1bb416f35708ddbaf2af44ac7b8f7e13b67aa6f7
Andrew Treptowgit config --global user.email andrew.treptow@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global core.safecrlf truevim filenamegit config --global user.name Andrew <andrew.treptow@gmail.com>
2021-09-03 22:39:35 -0700

    Passes through Test 9

--- src/recursion.js
+++ src/recursion.js
@@ -125,6 +125,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+    if (string === '') {
+        return string;
+      }
+      return reverse((string.slice(1))) + string[0];
 };
 
 // 10. Write a function that determines if a string is a palindrome.
